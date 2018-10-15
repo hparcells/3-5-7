d5b21ca34e7b4b50b1d60791e8b6cdea93d928b0
Hunter Parcells <hparcells@icloud.com>
2018-10-15 19:41:35 -0400

    Mark Updating

--- multiplayer/js/game.js
+++ multiplayer/js/game.js
@@ -8,8 +8,39 @@ var serverStatus = document.getElementById("serverStatus");
 var connectButton = document.getElementById("connectButton");
 var disconnectButton = document.getElementById("disconnectButton");
 var serverPanel = document.getElementById("serverPanel");
+var game = document.getElementById("game");
 
 var users = [];
+var marks = [
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+    {isMarked: false},
+];
+
+var player = 1;
+var marked = 0;
+var selectedRow = undefined;
+var win = false;
+
+const greenColor = "#4CAF50";
+
+var playerLabel = document.getElementById("player");
+
+const doneButton = document.getElementById("doneButton");
+const statusLabel = document.getElementById("status");
+const gameButton = document.getElementsByClassName("gameButton");
 
 function connectToServer() {
     console.log(`Connecting ${username.value} to http://${serverIP.value}:${serverPort.value}...`);
@@ -41,6 +72,7 @@ function connectToServer() {
         disconnectButton.disabled = true;
 
         serverPanel.style.display = "block";
+        game.classList.add("hidden");
 
         console.log(`Disconnected ${username.value} to http://${serverIP.value}:${serverPort.value}.`);
     });
@@ -49,6 +81,14 @@ function connectToServer() {
         users = data;
         
         if(users.length <= 2) {
+            if(users.length === 2) {
+                game.classList.remove("hidden");
+
+                playerLabel.innerHTML = users[0];
+            }else {
+                game.classList.add("hidden");
+            }
+
             console.log(users);
         }else if(users.length > 2 && username.value === users[2]){
             socket.emit("playerDisconnect", username.value);
@@ -56,14 +96,39 @@ function connectToServer() {
             socket.close();
         }
     });
+
+    socket.on("markUpdate", function(className, idName){
+        var mark = document.getElementById(idName);
+        var classes = className.split(" ");
+        var row = classes[1].substring(3);
+
+        if(marks[parseInt(idName) - 1].isMarked) return;
+
+        if(marked === 0) {
+            selectedRow = row;  
+            
+            doneButton.style.backgroundColor = greenColor;
+            doneButton.innerHTML = "Next Player";
     
-    socket.on("lostPlayer", function(data){
+            mark.classList.add("red");
+            marks[parseInt(idName) - 1].isMarked = true;
+            marked++;
+        }else {
+            if(row === selectedRow) {
+                mark.classList.add("red");
+                marks[parseInt(idName) - 1].isMarked = true;
+                marked++;
+            }
+        }
+    
+        checkWin();
 
+        socket.emit("updatedMarkList", marks);
     });
 
-    socket.on("updatedMark", function(data){
-
-    });
+    socket.on("updatedMarks", function(data) {
+        marks = data;
+    })
 
     socket.on("switchTurns", function(data){
 
@@ -77,4 +142,52 @@ function connectToServer() {
 function disconnectFromServer() {
     socket.emit("playerDisconnect", username.value);
     socket.close();
+}
+
+function doLogic(className, idName) {
+    socket.emit("updatedMark", className, idName);
+}
+
+function checkWin() {
+    if(allMarks()) {
+        var winningPlayer;
+
+        if(player === 1) {
+            winningPlayer = 2;
+
+            statusLabel.style.color = "blue";
+        }else {
+            winningPlayer = 1;
+            statusLabel.style.color = "red";
+        }
+
+        statusLabel.innerHTML = `Player ${users[winningPlayer - 1]} Wins!`;
+
+        doneButton.style.backgroundColor = greenColor;
+        doneButton.innerHTML = "Reset Game"
+
+        win = true;
+
+        return true;
+    }
+
+    return false;
+}
+
+function allMarks() {
+    return marks[0].isMarked === true &&
+            marks[1].isMarked === true &&
+            marks[2].isMarked === true &&
+            marks[3].isMarked === true &&
+            marks[4].isMarked === true &&
+            marks[5].isMarked === true &&
+            marks[6].isMarked === true &&
+            marks[7].isMarked === true &&
+            marks[8].isMarked === true &&
+            marks[9].isMarked === true &&
+            marks[10].isMarked === true &&
+            marks[11].isMarked === true &&
+            marks[12].isMarked === true &&
+            marks[13].isMarked === true &&
+            marks[14].isMarked === true;
 }
